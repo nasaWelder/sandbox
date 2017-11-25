bf18bd45f9f96cc0b2ce8f114fe6e52c4f77d8cf
nasaWelder <r/nasa_welder>
2017-11-24 21:38:42 -0700

    added 6 choices

--- monero/keyfree.py
+++ monero/keyfree.py
@@ -39,6 +39,9 @@ import time
 WORDS = wordlist.english
 config = {"offset2":random.randint(0,len(WORDS)),
           "offset3":random.randint(0,len(WORDS)),
+          "offset4":random.randint(0,len(WORDS)),
+          "offset5":random.randint(0,len(WORDS)),
+          "offset6":random.randint(0,len(WORDS)),
           "words":WORDS,
           }
           
@@ -48,13 +51,19 @@ seed = []
 
 
 def getChoices(step):
-    one = wraplist(i)
-    two = wraplist(i+config["offset2"])
-    three = wraplist(i+config["offset3"])
+    one = wraplist(step)
+    two = wraplist(step+config["offset2"])
+    three = wraplist(step+config["offset3"])
+    four = wraplist(step+config["offset4"])
+    five = wraplist(step+config["offset5"])
+    six = wraplist(step+config["offset6"])
     
     stdscr.addstr(1, 0, " "*100)
-    stdscr.addstr(1, 0, "choose: %s\t%s\t%s" % (one,two,three))
-    return one,two,three
+    stdscr.addstr(0, 0, "                  1            2            3")
+    stdscr.addstr(1, 0, "choose: %12s  %12s  %12s" % (one,two,three))
+    stdscr.addstr(3, 0, "                  4            5            6")
+    stdscr.addstr(4, 0, "        %12s  %12s  %12s" % (four,five,six))
+    return one,two,three,four,five,six
     
 def wraplist(index):
     global config
@@ -63,10 +72,11 @@ def wraplist(index):
     
 def getSeed(blah):
     global i
+    global config
     stdscr.clear()
     #curses.echo()
     stdscr.keypad(True)
-    one,two,three = getChoices(i) 
+    one,two,three,four,five,six = getChoices(i) 
     
     while True:
         c = stdscr.getch()
@@ -76,43 +86,49 @@ def getSeed(blah):
             seed.append(two)
         elif c == ord('3'):
             seed.append(three)
+        elif c == ord('4'):
+            seed.append(four)
+        elif c == ord('5'):
+            seed.append(five)
+        elif c == ord('6'):
+            seed.append(six)
         elif c == ord('p'):
-            stdscr.addstr(5,0,"".join([i+" " for i in seed]))
+            stdscr.addstr(6,0,"".join([x+" " for x in seed]))
         elif c == ord('l'):
-            stdscr.addstr(4,0,"# of seed words entered: %s" % len(seed))
+            stdscr.addstr(5,0,"# of seed words entered: %s" % len(seed))
             stdscr.refresh()
             time.sleep(1.5)
-            stdscr.addstr(4,0," "*30)
+            stdscr.addstr(5,0," "*30)
             stdscr.refresh()
         elif c == ord("r"):
             i+=random.randint(0,len(config["words"]))
-            one,two,three = getChoices(i)
+            one,two,three,four,five,six = getChoices(i)
         elif c == ord('q'):
             break  # Exit the while loop
         elif c == curses.KEY_DOWN:
             i+=1
-            one,two,three = getChoices(i)
+            one,two,three,four,five,six = getChoices(i)
         elif c == curses.KEY_UP:
             i-=1
-            one,two,three = getChoices(i)
+            one,two,three,four,five,six = getChoices(i)
         elif c == ord("z"):
             i+=1
-            one,two,three = getChoices(i)
+            one,two,three,four,five,six = getChoices(i)
         elif c == ord("a"):
             i-=1
-            one,two,three = getChoices(i)
+            one,two,three,four,five,six = getChoices(i)
         elif c == ord("x"):
             i+=random.randint(10,20)
-            one,two,three = getChoices(i)
+            one,two,three,four,five,six = getChoices(i)
         elif c == ord("s"):
             i-=random.randint(10,20)
-            one,two,three = getChoices(i)
+            one,two,three,four,five,six = getChoices(i)
         elif c == ord("c"):
             i+=random.randint(100,202)
-            one,two,three = getChoices(i)
+            one,two,three,four,five,six = getChoices(i)
         elif c == ord("d"):
             i-=random.randint(100,200)
-            one,two,three = getChoices(i)
+            one,two,three,four,five,six = getChoices(i)
         else:
             pass
 
